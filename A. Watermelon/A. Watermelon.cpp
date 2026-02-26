dddea1e84590e52bf254a780aa642b77701c3ba5
Shlok-Bhakta <shlok@spud>
2026-02-26 12:11:31 -0600

    remove bins

--- A. Watermelon/A. Watermelon.cpp
+++ A. Watermelon/A. Watermelon.cpp
@@ -15,57 +15,66 @@ using namespace std;
 #define int long long
 
 typedef unsigned long long ull;
-typedef pair<int,int> pii;
+typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vpii;
 typedef vector<vi> vvi;
 
-#define rep(i,n)     for(int i=0;i<(int)(n);i++)
-#define rep1(i,n)    for(int i=1;i<=(int)(n);i++)
-#define rrep(i,n)    for(int i=(int)(n)-1;i>=0;i--)
-#define fore(i,a,b)  for(int i=(a);i<(int)(b);i++)
-#define each(x,v)    for(auto& x:v)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+#define rrep(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
+#define fore(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define each(x, v) for (auto &x : v)
 
-#define pb    push_back
-#define eb    emplace_back
-#define all(x) (x).begin(),(x).end()
-#define sz(x)  (int)(x).size()
-#define F     first
-#define S     second
-#define nl    "\n"
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define nl "\n"
 
-#define chmin(a,b) a=min(a,b)
-#define chmax(a,b) a=max(a,b)
+#define chmin(a, b) a = min(a, b)
+#define chmax(a, b) a = max(a, b)
 
-#define yn(b)      cout<<((b)?"YES":"NO")<<nl
-#define printv(v)  for(auto& _x:v)cout<<_x<<" ";cout<<nl
-#define readv(v,n) v.resize(n);rep(i,n)cin>>v[i]
-#define sumv(v)    accumulate(all(v),0LL)
-#define minv(v)    *min_element(all(v))
-#define maxv(v)    *max_element(all(v))
-#define rsort(v)   sort((v).rbegin(),(v).rend())
-#define tc         int _t;cin>>_t;while(_t--)
+#define yn(b) cout << ((b) ? "YES" : "NO") << nl
+#define printv(v)                                                              \
+  for (auto &_x : v)                                                           \
+    cout << _x << " ";                                                         \
+  cout << nl
+#define readv(v, n)                                                            \
+  v.resize(n);                                                                 \
+  rep(i, n) cin >> v[i]
+#define sumv(v) accumulate(all(v), 0LL)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define tc                                                                     \
+  int _t;                                                                      \
+  cin >> _t;                                                                   \
+  while (_t--)
 
-template<typename T>
-inline int idx(const vector<T>& v,int i){return i<0?(int)v.size()+i:i;}
+template <typename T> inline int idx(const vector<T> &v, int i) {
+  return i < 0 ? (int)v.size() + i : i;
+}
 
 #ifdef LOCAL_DEBUG
-#define dbg(x) cerr<<#x<<" = "<<(x)<<nl
+#define dbg(x) cerr << #x << " = " << (x) << nl
 #else
 #define dbg(x)
 #endif
 
-const int INF  = 2e18;
-const int MOD  = 1e9+7;
+const int INF = 2e18;
+const int MOD = 1e9 + 7;
 
-void solve(){
+void solve() {
   int x = 0;
   cpp_dump(x);
 }
 
-int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // tc{ solve(); }
-    solve();
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  // tc{ solve(); }
+  solve();
 }
